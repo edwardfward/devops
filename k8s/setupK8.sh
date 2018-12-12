fa296fbd8eeb99f30449a8e5e6ce4805e8e5f5d4
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2018-12-12 16:26:35 -0500

    forcing copy overwrite

--- k8s/setupK8.sh
+++ k8s/setupK8.sh
@@ -77,7 +77,7 @@ if [[ $NODE_TYPE = "master" ]]; then
     sudo kubeadm init --pod-network-cidr=10.244.0.0/16 | sudo tee $HOME/kubeinit.out
     grep '^  kubeadm join' $HOME/kubeinit.out > kubenodetoken
     mkdir -p $HOME/.kube
-    sudo cp -irf /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo cp -rf /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
 
